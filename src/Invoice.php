ddbee8bfda93610481f405be71af0bb008714ff0
Anshor J <lordlexa10@gmail.com>
2024-04-29 09:14:56 +0700

    Add invoice blade and pdf funcionalities

---
+++ src/Invoice.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Inisiatif\Package\Template;
+
+use Illuminate\Http\Response;
+use Barryvdh\Snappy\PdfWrapper;
+use Inisiatif\Package\Template\Bridge\Donation;
+use Inisiatif\Package\Template\Bridge\Donor;
+
+final class Invoice
+{
+    private PdfWrapper $pdf;
+
+    public function __construct(PdfWrapper $pdf)
+    {
+        $this->pdf = $pdf;
+    }
+
+    public function make(Donor $donor, Donation $donation, array $details): self
+    {
+        $this->pdf->loadView('inisiatif::prints.invoice', compact('donation', 'donor', 'details'))
+            ->setPaper('A4')
+            ->setOption('margin-bottom', '4mm')
+            ->setOption('margin-left', '2mm')
+            ->setOption('margin-right', '2mm')
+            ->setOption('margin-top', '4mm')
+            ->setOption('page-height', '210mm')
+            ->setOption('page-width', '148mm');
+
+        return $this;
+    }
+
+    public function download(string $fileName): Response
+    {
+        return $this->pdf->download($fileName);
+    }
+
+    public function inline(string $fileName): Response
+    {
+        return $this->pdf->inline($fileName);
+    }
+
+    public function output(): string
+    {
+        return $this->pdf->output();
+    }
+}
