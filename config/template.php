43967a685daed8d34520062bde686e911a12d1cc
Anshor J <lordlexa10@gmail.com>
2024-07-29 17:00:59 +0700

    Add comment

--- config/template.php
+++ config/template.php
@@ -3,6 +3,6 @@
 declare(strict_types=1);
 
 return [
-    'modify_pdf_pin' => env('MODIFY_PDF_PIN'),
-    'use_arcfour_fallback' => env('USE_ARCFOUR_FALLBACK', true),
+    'modify_pdf_pin' => env('MODIFY_PDF_PIN'), // PIN UNTUK MENGUBAH PDF
+    'use_arcfour_fallback' => env('USE_ARCFOUR_FALLBACK', true), // FALLBACK UNTUK IMPLEMENTASI RC4-40. ATUR MENJADI TRUE JIKA MENGGUNAKAN OPENSSL 3.0.
 ];
